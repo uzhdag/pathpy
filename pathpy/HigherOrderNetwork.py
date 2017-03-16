4101bf897f02234f73aca6c654d99320f199857c
Ingo Scholtes <ischoltes@ethz.ch>
2017-03-16 22:09:12 +0100

    fixed issue #9

--- pathpy/HigherOrderNetwork.py
+++ pathpy/HigherOrderNetwork.py
@@ -67,7 +67,7 @@ class HigherOrderNetwork:
 
         assert method == 'FirstOrderTransitions' or method == 'KOrderPi', 'Error: unknown method to build null model'
 
-        assert max(paths.paths.keys())>=k, 'Error: constructing a model of order k requires paths of at least length k'
+        assert len(paths.paths.keys())>0 and max(paths.paths.keys())>=k, 'Error: constructing a model of order k requires paths of at least length k'
         
         ## The order of this HigherOrderNetwork
         self.order = k
