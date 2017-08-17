0af7904b095e527fc5452f0ef641ba372f00e910
Ingo Scholtes <ischoltes@ethz.ch>
2017-08-17 09:03:07 -0300

    added unit test for HigherOrderNetwork

---
+++ tests/test_HigherOrderNetwork.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+    pathpy is an OpenSource python package for the analysis of sequential data
+    on pathways and temporal networks using higher- and multi order graphical models
+
+    Copyright (C) 2016-2017 Ingo Scholtes, ETH Zürich
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Contact the developer:
+
+    E-mail: ischoltes@ethz.ch
+    Web:    http://www.ingoscholtes.net
+"""
+
+import pathpy as pp
+import pytest
+
+slow = pytest.mark.slow
+
+def test_degrees(path_from_edge_file):
+    hon_1 = pp.HigherOrderNetwork(path_from_edge_file, k=1)
+    node_map = hon_1.getNodeNameMap()
+    expected_degrees = {'1': 52, '2' : 0, '3': 2, '5': 5}
+    d = hon_1.degrees()
+    degrees = {}
+    for v in node_map:
+        degrees[v] = d[node_map[v]]
+    assert degrees == expected_degrees, \
+        "Wrong degree calculation in HigherOrderNetwork"
