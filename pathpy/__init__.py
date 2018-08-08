bb6d457bcd5fb8464a45bff2952f6bdd2ef11ffa
Ingo Scholtes <ischoltes@ethz.ch>
2018-08-08 19:57:17 +0200

    clearer import structure

--- pathpy/__init__.py
+++ pathpy/__init__.py
@@ -5,10 +5,16 @@ __email__ = 'scholtes@ifi.uzh.ch'
 __version__ = '2.0.0'
 
 from .classes import *
-from .algorithms import *
 from .path_extraction import *
 from .visualisation import *
 import pathpy.utils
+
+from .algorithms import centralities
+from .algorithms import shortest_paths
+from .algorithms import path_measures
+from .algorithms import components
+from .algorithms import infomap
+from .algorithms import spectral
 
 global ENABLE_MULTICORE_SUPPORT
 ENABLE_MULTICORE_SUPPORT = False
--- pathpy/algorithms/__init__.py
+++
@@ -1,6 +0,0 @@
-from .centralities import *
-from .shortest_paths import *
-from .path_measures import *
-from .components import *
-from .infomap import *
-from .spectral import *
