a593ddcfe8d774686e6ee9bb3d6a367cb780d39b
Ingo Scholtes <ischoltes@ethz.ch>
2018-08-08 19:51:27 +0200

    submodule structure in algorithms

--- pathpy/algorithms/__init__.py
+++ pathpy/algorithms/__init__.py
@@ -1,18 +1,6 @@
-from .centralities import betweenness_centrality
-from .centralities import closeness_centrality
-from .centralities import eigenvector_centrality
-from .centralities import node_traversals
-from .centralities import pagerank
-from .centralities import rank_centralities
-from .centralities import visitation_probabilities
-
-from .shortest_paths import diameter
-from .shortest_paths import distance_matrix
-from .shortest_paths import shortest_paths
-
-from .components import connected_components
-from .components import reduce_to_gcc
-
-from .infomap import find_communities_sa
-from .infomap import modular_description_length
+from .centralities import *
+from .shortest_paths import *
+from .path_measures import *
+from .components import *
+from .infomap import *
 from .spectral import *
